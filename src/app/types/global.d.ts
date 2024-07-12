927e4e239a2e2e119546d2c5850a196fb06dd400
Nikita <neavinstasi@icloud.com>
2024-07-12 23:50:43 +0300

    minot fixes

---
+++ src/app/types/global.d.ts
@@ -0,0 +1,21 @@
+declare module '*.{ts,tsx}' {
+  import 'vite/client';
+}
+
+declare module '*.scss' {
+  interface IClassNames {
+    [className: string]: string;
+  }
+  const classNames: IClassNames;
+  export = classNames;
+}
+
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg' {
+  import type React from 'react';
+
+  const SVG: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default SVG;
+}
