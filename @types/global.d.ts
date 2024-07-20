cd96efa1afe9f837e8f0b8b6e1b1ef92c81bb4de
Nikita <neavinstasi@icloud.com>
2024-07-20 10:43:43 +0300

    edit global.d.ts file

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -3,10 +3,10 @@ declare module '*.{ts,tsx}' {
 }
 
 declare module '*.scss' {
-  interface IClassNames {
+  interface ClassNames {
     [className: string]: string;
   }
-  const classNames: IClassNames;
+  const classNames: ClassNames;
   export = classNames;
 }
 
