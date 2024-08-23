b6756bb8f18b8aed974b725551055eea444d851a
Nikita <neavinstasi@icloud.com>
2024-08-23 21:02:30 +0300

    refactor project config

--- vite.config.ts
+++ vite.config.ts
@@ -9,12 +9,14 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@ui': path.resolve(__dirname, './src/shared/components/ui'),
-      '@components': path.resolve(__dirname, './src/shared/components'),
+      '@icons/*': path.resolve(__dirname, './src/shared/assets/icons/*'),
+      '@images/*': path.resolve(__dirname, './src/shared/assets/images/*'),
+      '@components': path.resolve(__dirname, './src/components'),
       '@constants': path.resolve(__dirname, './src/shared/constants'),
       '@api': path.resolve(__dirname, './src/shared/api'),
       '@pages': path.resolve(__dirname, './src/pages'),
       '@hooks': path.resolve(__dirname, './src/shared/hooks'),
+      '@store': path.resolve(__dirname, './src/shared/store'),
     },
   },
 });
